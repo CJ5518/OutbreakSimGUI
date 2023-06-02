06a7344c8f8de1fd1725a11d096a4735934b90c3
CJ5518 <carsonrueber@gmail.com>
2023-06-02 16:58:20 -0700

    slurmUtils use os execute for everything

--- slurmUtils.lua
+++ slurmUtils.lua
@@ -5,18 +5,15 @@ local module = {};
 --Run command n times
 function module.runJob(command, n)
 	local filename = "~/mapsimgui/.slurmfiles/OutbreakSim-" .. os.time() .. ".slurm";
-	local slurmFile = io.open(filename, "w");
-	slurmFile:write(string.format(
-[[#!/bin/bash
-
-#SBATCH -p tiny
-
-cd $SLURM_SUBMIT_DIR
-
+	os.execute(string.format("echo %s > %s",
+[[#!/bin/bash\\
+\\
+#SBATCH -p tiny\\
+\\
+cd $SLURM_SUBMIT_DIR\\
+\\
 %s
-]], command));
-	slurmFile:flush();
-	slurmFile:close();
+]], command, filename));
 	os.execute(string.format("sbatch -a 1-%d %s", n, filename))
 end
 
