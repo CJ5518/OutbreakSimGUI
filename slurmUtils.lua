f900aa6fd87c8f41c226dca3e9efabef9d71d17e
CJ5518 <carsonrueber@gmail.com>
2023-06-02 17:11:28 -0700

    Fix slurmUtils for real this time

--- slurmUtils.lua
+++ slurmUtils.lua
@@ -5,7 +5,8 @@ local module = {};
 --Run command n times
 function module.runJob(command, n)
 	local filename = "~/mapsimgui/.slurmfiles/OutbreakSim-" .. os.time() .. ".slurm";
-	os.execute(string.format("printf %s > %s","#!/bin/bash\n\n#SBATCH -p tiny\n\ncd $SLURM_SUBMIT_DIR\n\n%s", command, filename));
+	local fileText = string.format("#!/bin/bash\\n\\n#SBATCH -p tiny\\n\\ncd $SLURM_SUBMIT_DIR\\n\\n%s", command);
+	os.execute(string.format("printf %s > %s",fileText, filename));
 	os.execute(string.format("sbatch -a 1-%d %s", n, filename))
 end
 
