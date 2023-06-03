9a56ff161c2244411756a136cc738b3a802fee8f
CJ5518 <carsonrueber@gmail.com>
2023-06-02 17:23:06 -0700

    Revert bad decisions

--- slurmUtils.lua
+++ slurmUtils.lua
@@ -5,8 +5,15 @@ local module = {};
 --Run command n times
 function module.runJob(command, n)
 	local filename = "~/mapsimgui/.slurmfiles/OutbreakSim-" .. os.time() .. ".slurm";
-	local fileText = string.format("#!/bin/bash\\n\\n#SBATCH -p tiny\\n\\ncd $SLURM_SUBMIT_DIR\\n\\n%s", command);
-	os.execute(string.format("printf \"%s\" > \"%s\"",fileText, filename));
+	os.execute(string.format("echo %s > %s",
+[[#!/bin/bash\\
+\\
+#SBATCH -p tiny\\
+\\
+cd $SLURM_SUBMIT_DIR\\
+\\
+%s
+]], command, filename));
 	os.execute(string.format("sbatch -a 1-%d %s", n, filename))
 end
 
