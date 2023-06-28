d1f433f541d2a4d680c4a0cf94445769b1e1295e
CJ5518 <carsonrueber@gmail.com>
2023-06-28 13:48:48 -0700

    Update installer for new version

--- installSelf.sh
+++ installSelf.sh
@@ -10,8 +10,8 @@ cd mapsimgui
 mkdir output
 mkdir .slurmfiles
 mkdir mapsim
-wget https://github.com/CJ5518/MapSimulation1/releases/download/Linux-2023.6.27/LinuxHeadless-6.27.2023.tar
-tar -xf LinuxHeadless-6.27.2023.tar -C mapsim
-rm LinuxHeadless-6.27.2023.tar
+wget https://github.com/CJ5518/MapSimulation1/releases/download/Linux-2023.6.28/LinuxHeadless-6.28.2023.tar
+tar -xf LinuxHeadless-6.28.2023.tar -C mapsim
+rm LinuxHeadless-6.28.2023.tar
 # The sif file hasn't changed since this
 wget https://github.com/CJ5518/MapSimulation1/releases/download/Build-2023.6.2/mapsimgui.sif
